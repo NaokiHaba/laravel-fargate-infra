df4f83d6230f7f778e29c782d43da511c4bef8ac
NaokiHaba <naosprintrunner6385@gmail.com>
2023-02-10 02:15:54 +0900

    feat: ECR完成まで

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,5 +1,12 @@
 module "nginx" {
   source = "../../../../modules/ecr"
 
-  name = "example-prod-foobar-nginx"
+  name = "${local.name_prefix}-${local.service_name}-nginx"
+}
+
+# PHP用ECR
+module "php" {
+  source = "../../../../modules/ecr"
+
+  name = "${local.name_prefix}-${local.service_name}-php"
 }
